b3e0f28ce7c49fb297d0557f64db09ad5b3b0800
jlchesp <43406974+jlchesp@users.noreply.github.com>
2020-09-18 08:10:16 +0200

    Traducción a inglés

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,11 +1,13 @@
 import {Response, Request, NextFunction} from 'express';
 import Token from '../classes/token';
 
-
+// A middleware is a function that we want to execute before a certain function finishes executing
+// It is an intermediate step
 export const verifyToken = (req: any, res: Response, next: NextFunction) => {
 
     const userToken = req.get('x-token') || '';
-
+    
+    // We check the user token and make the previous function (in our case, the update user) continue with the 'next ()' function
     Token.checkToken(userToken)
         .then( (decoded: any) => {
             req.user = decoded.user;
